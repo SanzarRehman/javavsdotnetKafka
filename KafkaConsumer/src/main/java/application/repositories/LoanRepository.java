1dba8e4c7e6e8a00aeb2ebf1f1261c1e826302bf
sanzar rahman <sanzar.rahman@bracits.com>
2025-07-03 19:21:43 +0600

    Remove unused Kafka and JPA related classes to streamline the application

--- KafkaConsumer/src/main/java/application/repositories/LoanRepository.java
+++
@@ -1,8 +0,0 @@
-package application.repositories;
-
-import application.entities.Loan;
-import java.util.UUID;
-import org.springframework.data.repository.CrudRepository;
-
-public interface LoanRepository extends CrudRepository<Loan, UUID> {
-}
